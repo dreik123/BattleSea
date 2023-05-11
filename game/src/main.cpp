786210157c7cb8172c1a18dcc7a8454ae45f3d67
Dmytro Skorobohatskyi <skorobogatskiidima@gmail.com>
2023-05-11 23:36:20 +0300

    Organized the project structure with VS usage

---
+++ game/src/main.cpp
@@ -0,0 +1,28 @@
+﻿// BattleSea.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+class GameController;
+
+struct IGame {};
+class BattleSeaGame : public IGame {};
+
+struct IView {};
+class TerminalPresenter : public IView {};
+
+GameController* CreateController(IGame* game, IView* presenter) { return nullptr; }
+
+struct GameConfig {}; // containing info about amount and type of ships
+IGame* CreateGame(const GameConfig&) { return nullptr; }
+
+IView* CreatePresenter() { return new TerminalPresenter(); }
+
+
+int main(int argc, char* argv[])
+{
+    // Classic MVC
+    IGame* game = CreateGame(GameConfig());
+    IView* presenter = CreatePresenter();
+    GameController* controller = CreateController(game, presenter);
+
+    return 0;
+}
