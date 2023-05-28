1d9a4b161bf9d352b303ff03e310a67b7f3ffafc
dreik123 <andrey149814@gmail.com>
2023-05-28 12:32:25 +0300

    reviewed

--- game/src/WarShip.cpp
+++ game/src/WarShip.cpp
@@ -10,13 +10,13 @@ WarShip::WarShip(const std::vector<CellIndex>& cells)
     }
 }
 
-std::vector<CellIndex> WarShip::GetOccupiedCells() const
+const std::vector<CellIndex> WarShip::GetOccupiedCells() const
 {
     std::vector<CellIndex> cells;
     for(const auto& shipdeck : ShipDecks)
     {
         std::transform(ShipDecks.cbegin(), ShipDecks.cend(), std::back_inserter(cells),
-            [](std::pair<CellIndex, bool> p) { return p.first; });
+                       [](const std::pair<CellIndex, bool>& p) { return p.first; });
     }
     return cells;
 }
@@ -28,15 +28,15 @@ int WarShip::GetOccupiedCellsAmount() const
 
 bool WarShip::IsDestroyed() const 
 {
-    const int count = std::count_if(ShipDecks.begin(), ShipDecks.end(), 
-                               [](std::pair<CellIndex, bool> p){ return p.second == true; }); 
+    const int count = std::count_if(ShipDecks.cbegin(), ShipDecks.cend(), 
+                                    [](const std::pair<CellIndex, bool>& p){ return p.second == true; }); 
     return count == 0;
 }
 
 bool WarShip::DoesOccupyTheCell(const CellIndex& cell) const 
 {
-    auto it = std::find_if(ShipDecks.begin(), ShipDecks.end(), 
-                           [&cell](const std::pair<CellIndex, bool> p) { return p.first == cell; });
+    auto it = std::find_if(ShipDecks.cbegin(), ShipDecks.cend(), 
+                           [&cell](const std::pair<CellIndex, bool>& p) { return p.first == cell; });
     return it != ShipDecks.end();
 }
 
@@ -45,7 +45,7 @@ void WarShip::ShootShipAtCell(const CellIndex& cell)
     if (DoesOccupyTheCell(cell))
     {
         auto it = std::find_if(ShipDecks.begin(), ShipDecks.end(), 
-                               [&cell](const std::pair<CellIndex, bool> p) { return p.first == cell; });
+                               [&cell](const std::pair<CellIndex, bool>& p) { return p.first == cell; });
         it->second = false;
     }
 }
--- game/src/WarShip.h
+++ game/src/WarShip.h
@@ -7,13 +7,13 @@ class WarShip
 public:
     WarShip(const std::vector<CellIndex>& cells);
 
-    std::vector<CellIndex> GetOccupiedCells() const;
-    int GetOccupiedCellsAmount() const; // aka ship type
+    [[nodiscard]] const std::vector<CellIndex> GetOccupiedCells() const;
+    [[nodiscard]] int GetOccupiedCellsAmount() const; // aka ship type
 
     //bool IsDamaged() const; // optional
-    bool IsDestroyed() const;
+    [[nodiscard]] bool IsDestroyed() const;
 
-    bool DoesOccupyTheCell(const CellIndex& cell) const;
+    [[nodiscard]] bool DoesOccupyTheCell(const CellIndex& cell) const;
     void ShootShipAtCell(const CellIndex& cell);
     //bool TryShootShipAtCell(const CellIndex& cell); // optional
 
