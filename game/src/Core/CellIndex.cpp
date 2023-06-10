704b26d888af4f3ab4e40ebbc48c9539fc385e06
dreik123 <andrey149814@gmail.com>
2023-06-10 11:58:10 +0300

    fixed char_utilities

--- game/src/Core/CellIndex.cpp
+++ game/src/Core/CellIndex.cpp
@@ -3,43 +3,43 @@
 namespace char_utilities
 {
     // Compile time analog of tolower()
-    static constexpr char CharToLower(const char value) {
+    static constexpr char charToLower(const char value) {
         return (value >= 'A' && value <= 'Z') ? value + ('a' - 'A') : value;
     }
 
     // Compile time analog of isdigit()
-    static constexpr char IsDigit(const char value) {
+    static constexpr char isDigit(const char value) {
         return value >= '0' && value <= '9';
     }
 
     // Compile time analog of isalpha()
-    static constexpr char IsAlpha(const char value) {
+    static constexpr char isAlpha(const char value) {
         return (value >= 'A' && value <= 'Z') || (value >= 'a' && value <= 'z');
     }
 
-    static constexpr int8_t DigitCharToInt(const char value)
+    static constexpr int8_t digitCharToInt(const char value)
     {
-        assert(IsDigit(value));
+        assert(isDigit(value));
         return value - '0';
     }
 
-    static constexpr int8_t LetterCharToInt(const char value, bool isCaseInsensetive = true)
+    static constexpr int8_t letterCharToInt(const char value, bool isCaseInsensetive = true)
     {
-        assert(IsAlpha(value));
-        const char val = isCaseInsensetive ? CharToLower(value) : value;
+        assert(isAlpha(value));
+        const char val = isCaseInsensetive ? charToLower(value) : value;
 
         return val - 'a';
     }
 
     // InValue is in [0-9]. 0 means 'a'
-    static constexpr char IndexToLetterChar(const int value, bool isUpperCase)
+    static constexpr char indexToLetterChar(const int value, bool isUpperCase)
     {
         assert(value >= 0);
         return value + (isUpperCase ? 'A' : 'a');
     }
 
     // TODO constexpr for static_assert support (will be done in the next commits)
-    static const std::string IndexToDigitStr(const int value)
+    static const std::string indexToDigitStr(const int value)
     {
         assert(value >= 0);
         return std::to_string(value + 1); // convert to user-friendly value
@@ -61,11 +61,11 @@ CellIndex::CellIndex(const std::string& coord)
         (coord.size() == 3 && isalpha(coord[0]) && coord[1] == '1' && coord[2] == '0')
     );
 
-    m_internalCoordinates.first = char_utilities::LetterCharToInt(coord[0], true);
+    m_internalCoordinates.first = char_utilities::letterCharToInt(coord[0], true);
     assert(m_internalCoordinates.first >= 0 && m_internalCoordinates.first < GridRowCount);
     if (coord.size() == 2)
     {
-        m_internalCoordinates.second = char_utilities::DigitCharToInt(coord[1]) - 1; // -1 to handle array index and view difference
+        m_internalCoordinates.second = char_utilities::digitCharToInt(coord[1]) - 1; // -1 to handle array index and view difference
     }
     else if (coord[1] == '1' && coord[2] == '0')
     {
@@ -81,8 +81,8 @@ CellIndex::CellIndex(const std::string& coord)
 const std::string CellIndex::toString(const bool isUpperCase) const
 {
     std::string result;
-    result.push_back(char_utilities::IndexToLetterChar(m_internalCoordinates.first, isUpperCase));
-    result += char_utilities::IndexToDigitStr(m_internalCoordinates.second);
+    result.push_back(char_utilities::indexToLetterChar(m_internalCoordinates.first, isUpperCase));
+    result += char_utilities::indexToDigitStr(m_internalCoordinates.second);
 
     return result;
 }
@@ -93,15 +93,15 @@ constexpr void CellIndexTests()
 {
     using namespace char_utilities;
 
-    static_assert(LetterCharToInt('a') == 0);
-    static_assert(LetterCharToInt('b') == 1);
-    static_assert(CharToLower('C') == 'c');
-    static_assert(LetterCharToInt(CharToLower('C')) == 2);
-    static_assert(LetterCharToInt('C') == 2);
-    static_assert(LetterCharToInt('C', false) != 2);
+    static_assert(letterCharToInt('a') == 0);
+    static_assert(letterCharToInt('b') == 1);
+    static_assert(charToLower('C') == 'c');
+    static_assert(letterCharToInt(charToLower('C')) == 2);
+    static_assert(letterCharToInt('C') == 2);
+    static_assert(letterCharToInt('C', false) != 2);
 
-    static_assert(IndexToLetterChar(4, false) == 'e');
-    static_assert(IndexToLetterChar(5, true) == 'F');
+    static_assert(indexToLetterChar(4, false) == 'e');
+    static_assert(indexToLetterChar(5, true) == 'F');
     //static_assert(IndexToDigitStr(1) == "2");
     //static_assert(IndexToDigitStr(9) == "10");
 }
