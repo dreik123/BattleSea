c188f68e91a2fcaf64a4f5b4254eb33ef7bd6c7e
dreik123 <andrey149814@gmail.com>
2023-06-18 12:25:28 +0300

    added CellIndexMock

--- tests/WarShipTest.cpp
+++ tests/WarShipTest.cpp
@@ -1,2 +1,20 @@
 #include "pch.h"
+#include "Game/WarShip.h"
+#include "CellIndexMock.h"
+#include <gmock/gmock.h>
+#include <vector>
+#include <memory>
+
+class WarShipTest : public testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        auto temp = std::make_unique<testing::StrictMock<WarShip>>(m_mock);
+    }
+protected:
+    std::unique_ptr<WarShip> m_warShip;
+    testing::StrictMock<CellIndexMock>* m_mock;
+};
+
 
