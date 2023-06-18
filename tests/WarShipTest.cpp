0fabf699c04df9f21814978d31314199c8c87036
dreik123 <andrey149814@gmail.com>
2023-06-18 13:39:01 +0300

    fix tests

--- tests/WarShipTest.cpp
+++ tests/WarShipTest.cpp
@@ -5,16 +5,5 @@
 #include <vector>
 #include <memory>
 
-class WarShipTest : public testing::Test
-{
-protected:
-    void SetUp() override
-    {
-        auto temp = std::make_unique<testing::StrictMock<WarShip>>(m_mock);
-    }
-protected:
-    std::unique_ptr<WarShip> m_warShip;
-    testing::StrictMock<CellIndexMock>* m_mock;
-};
 
 
